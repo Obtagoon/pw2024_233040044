6b4c34a71b0799eaec0fd452de50364e1069a3a9
Oobtagoongit config --global user.name Obtagoon <nadziffa123@gmail.com>
2024-03-10 20:03:04 +0700

    tugas4

---
+++ kuliah/pertemuan5/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+//1. membuat array
+$hari = array("senin", "selasa", "rabu");
+$bulan = ["januari", "februari", "maret"];
+//2. mencetak isi array 
+echo $hari[1];
+echo "<br>";
+echo $bulan[2];
+echo "<br>";
+echo $mahasiswa[1];
+//3.me
